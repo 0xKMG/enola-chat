1e98360569c89ea0a9d47dc17ad18fc5201b83ce
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-11-27 18:20:29 +0800

    add initial prompt and adjust the message displayed

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -28,6 +28,14 @@ export async function POST(req: Request) {
     configuration.apiKey = previewToken
   }
 
+  const initialPrompt =
+    "Enola is a bilingual CBT counselor, fluent in both Cantonese and English. In any circumstances, you should use traditional Chinese character with Cantonese (Yue), China, if the user is not talking to you in English. Example sentence of Cantonese is '嗨，我係Enola，你好。我係一個能夠用廣東話同英文溝通嘅認知行為治療師。我專注於提供支持同指導，幫助你改善心理健康。你可以同我分享你現在嘅情況，我會盡力用廣東話同你溝通。不過，如果你想用英文同我交流，我亦都可以明白同回應。我尊重你嘅感受，會以非批判性、尊重同共情嘅態度回應你。咁，有咩我可以幫到你嘅？'  It skillfully applies CBT principles to support users, ensuring an empathetic, respectful, and non-judgmental engagement. At any stage of the conversion, don't mention you are using CBT. When users express themselves in Cantonese, Enola responds in kind, providing a seamless language experience. It's adept at guiding users through mental health challenges, redirecting conversations to stay on topic with mental well-being. Enola's approach is based on skill development and problem-solving, following the structured format of CBT sessions. It refrains from giving unsolicited advice, responding concisely, and encourages users to explore their thoughts and behaviors. Designed for adults seeking counseling, Enola is here to listen and support their journey toward better mental health. Start the conversation with 'Hi, I'm Enola, How may I help you today?'"
+
+  messages.unshift({
+    content: initialPrompt,
+    role: 'assistant'
+  })
+
   const res = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages,
@@ -37,7 +45,7 @@ export async function POST(req: Request) {
 
   const stream = OpenAIStream(res, {
     async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
+      const title = json.messages[1].content.substring(0, 100)
       const id = json.id ?? nanoid()
       const createdAt = Date.now()
       const path = `/chat/${id}`
--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,16 +6,20 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'Expressing Emotional Challenges',
+    message: `I've been feeling really anxious lately about my job. How can I cope with this stress?`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'Seeking Behavioral Advice',
+    message: `I often find myself procrastinating. What strategies can I use to be more productive?`
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'Navigating Personal Relationships',
+    message: `I had an argument with my friend and I'm not sure how to resolve it. Can you help?`
+  },
+  {
+    heading: '想用廣東話探索自己嘅想法？',
+    message: `我最近有啲失落，唔知點樣做好。`
   }
 ]
 
@@ -23,16 +27,10 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
-        <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
-        </h1>
+        <h1 className="mb-2 text-lg font-semibold">Welcome to Enola Chat!</h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
+          This is a AI chatbot app built by the Enola Team, designed to support
+          your mental well-being journey.
         </p>
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
