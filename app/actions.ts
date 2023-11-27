ed0bd08fe72035781149923b93d886e0fe7b2637
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-11-27 16:51:09 +0800

    fix: getChat couldn't get the correct id

--- app/actions.ts
+++ app/actions.ts
@@ -11,6 +11,7 @@ export async function getChats(userId?: string | null) {
   if (!userId) {
     return []
   }
+
   try {
     const pipeline = kv.pipeline()
     const chats: string[] = await kv.zrange(`user:chat:${userId}`, 0, -1, {
@@ -30,9 +31,8 @@ export async function getChats(userId?: string | null) {
 }
 
 export async function getChat(id: string, userId: string) {
-  let rawChat = await kv.hgetall<Chat>(`chat:${id}`)
-  const uid = await kv.hget(`chat:${id}`, 'userId')
-  const chat = { ...rawChat!, userId: uid }
+  const chat = await kv.hgetall<Chat>(`chat:${id}`)
+  console.log('Chat in getChat function:', chat)
 
   if (!chat || (userId && chat.userId !== `uid-${userId}`)) {
     return null
@@ -52,15 +52,14 @@ export async function removeChat({ id, path }: { id: string; path: string }) {
 
   const uid = await kv.hget<string>(`chat:${id}`, 'userId')
 
-  if (uid !== `uid-${session.user.sub}`) {
-    console.log('log uid and user.sub', uid, session.user.sub)
+  if (uid !== session?.user?.id) {
     return {
       error: 'Unauthorized'
     }
   }
 
   await kv.del(`chat:${id}`)
-  await kv.zrem(`user:chat:${session.user.sub}`, `chat:${id}`)
+  await kv.zrem(`user:chat:${session.user.id}`, `chat:${id}`)
 
   revalidatePath('/')
   return revalidatePath(path)
@@ -69,17 +68,13 @@ export async function removeChat({ id, path }: { id: string; path: string }) {
 export async function clearChats() {
   const session = await auth()
 
-  if (!session?.user?.sub) {
+  if (!session?.user?.id) {
     return {
       error: 'Unauthorized'
     }
   }
 
-  const chats: string[] = await kv.zrange(
-    `user:chat:${session.user.sub}`,
-    0,
-    -1
-  )
+  const chats: string[] = await kv.zrange(`user:chat:${session.user.id}`, 0, -1)
   if (!chats.length) {
     return redirect('/')
   }
@@ -87,7 +82,7 @@ export async function clearChats() {
 
   for (const chat of chats) {
     pipeline.del(chat)
-    pipeline.zrem(`user:chat:${session.user.sub}`, chat)
+    pipeline.zrem(`user:chat:${session.user.id}`, chat)
   }
 
   await pipeline.exec()
@@ -109,7 +104,7 @@ export async function getSharedChat(id: string) {
 export async function shareChat(chat: Chat) {
   const session = await auth()
 
-  if (!session?.user?.id || session.user.sub !== chat.userId) {
+  if (!session?.user?.id || session.user.id !== chat.userId) {
     return {
       error: 'Unauthorized'
     }
