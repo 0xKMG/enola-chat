6b18a63359d78d00b7125fb9f00d639fe4ef024b
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2023-12-03 18:35:03 +0800

    change temperature for consistency

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -38,7 +38,7 @@ export async function POST(req: Request) {
   const res = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages,
-    temperature: 0.7,
+    temperature: 0.2,
     stream: true
   })
 
